a14bcaa1045a03c127310d4ce8646b7767ce5774
riya patel <riya.syndell@gmail.com>
2023-04-11 11:36:23 +0530

    Initial Commit

--- config/db.js
+++ config/db.js
@@ -5,11 +5,11 @@ const { createPool } = require("mysql");
 const { resolve } = require("path");
 
 const pool = createPool({
-    host:"127.0.0.1",
-    user:"root",
-    password:"",
-    port:"3306",
-    database:"web_portal"
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    port: "3306",
+    database: "web_portal"
     //============live database connection=================//
     // host:"dbportal.crq074bejaot.ap-northeast-1.rds.amazonaws.com",
     // user:"admin",
@@ -24,32 +24,29 @@ const pool = createPool({
 
 })
 
-pool.getConnection((err)=>{
-    if(err)
-    {
+pool.getConnection((err) => {
+    if (err) {
         console.log("Database Connection Error");
     }
     console.log("Database Connected Successfully");
 });
 
-const executeQuery=(query, arraparams)=>{
-    return new Promise((resolve,rejects)=>{
-        try{
-            pool.query(query, arraparams,(err,data)=>{
-                if(err)
-                {
+const executeQuery = (query, arraparams) => {
+    return new Promise((resolve, rejects) => {
+        try {
+            pool.query(query, arraparams, (err, data) => {
+                if (err) {
                     console.log('Error in executing query');
                     rejects(err);
                 }
                 resolve(data);
             })
         }
-        catch(err)
-        {
+        catch (err) {
             rejects(err);
         }
     });
 }
 
 //module.exports=pool;
-module.exports={executeQuery};
+module.exports = { executeQuery };
